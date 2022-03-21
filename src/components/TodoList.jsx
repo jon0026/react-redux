c2f6bfb57010463b5368fc42e0512b556a867330
Juma81 <jonathanumana10@gmail.com>
2022-03-20 23:34:36 -0600

    Taller 10 Redux

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import NewTodoForm from './NewTodoForm';
+import TodoListItem from './TodoListItem';
+import './TodoList.css';
+const TodoList = ({ todos = [{text:"New Todo"}] }) => ( 
+    <div className="list-wrapper">
+        <NewTodoForm />
+        {todos.map((todo, index) => (
+          <TodoListItem key={index} todo={todo} />  
+        ))}
+    </div>
+);
+
+export default TodoList;
